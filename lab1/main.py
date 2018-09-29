574f28caa8678f34bff7b8005a98d5c813288895
Daria Firsova <tonitruif@gmail.com>
2018-09-29 14:54:13 +0300

    fixed

--- lab1/main.py
+++ lab1/main.py
@@ -1,11 +1,14 @@
 
-import pandas
+import pandas as pd
 
 
 def get():
-    #data = pandas.read_csv('lines1.csv', error_bad_lines=False)
-    Lines = pandas.read_csv('lines.csv')
-
+    #data = pd.read_csv('lines1.csv', error_bad_lines=False)
+    Lines = pd.read_csv('lines.csv')
+    Stations = pd.read_csv('stations.csv')
+    Exits = pd.read_csv('exits.csv', sep = ';')
+    print(Exits)
+    print(Stations)
     print(Lines)
 
 if __name__ == "__main__":
