dcac398e7ed2f6cf151af71e38f4383eb22628e8
Daria Firsova <tonitruif@gmail.com>
2018-11-11 01:43:40 +0300

    +

--- lab1/TacblesCreate.sql
+++ lab1/TacblesCreate.sql
@@ -2,11 +2,40 @@ USE Metro
 GO
 
 CREATE TABLE StationsT
-	(Station nvarchar(50) NOT NULL,
-	Line nvarchar(70) NOT NULL,
-	Admarea nvarchar(70) NOT NULL,
+	(Station nvarchar(100) NOT NULL,
+	Line nvarchar(100) NOT NULL,
+	Admarea nvarchar(100) NOT NULL,
 	global_id int NOT NULL,
-	District nvarchar(70) NOT NULL,
-	Status nvarchar (20) NOT NULL,
+	District nvarchar(100) NOT NULL,
+	Status nvarchar (40) NOT NULL,
 	Id int NOT NULL)
+GO
+--BuildDate;LineID;MaxPassengers;MaxVagon;Train
+--1993;1;1054;12;Ока
+CREATE TABLE TrainsT
+	(BuildDate int NOT NULL,
+	LineID int NOT NULL, 
+	MaxPassengers int NOT NULL,
+	MaxVagon int NOT NULL,
+	Train nvarchar(15) NOT NULL)
+GO
+
+
+--Line;LineID;Passangers;Vagons
+--Сокольническая линия;1;122;2
+CREATE TABLE LinkerT
+	(Line nvarchar(100) NOT NULL,
+	LineID int NOT NULL, 
+	Passengers int NOT NULL,
+	Vagons int NOT NULL)
+GO
+
+--Line;LineID;global_id;Status;
+--Сокольническая линия;1;62921363;действует;
+
+CREATE TABLE LinesT
+	(Line nvarchar(100) NOT NULL,
+	LineID int NOT NULL,
+	global_id int NOT NULL,
+	Status nvarchar(25) NOT NULL)
 GO
