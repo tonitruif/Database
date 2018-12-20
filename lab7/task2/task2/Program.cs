7954935892e51147655ae2d345fb55b6706d5f73
Daria Firsova <tonitruif@gmail.com>
2018-12-20 22:37:55 +0300

    task 2 lab7

--- lab7/task2/task2/Program.cs
+++ lab7/task2/task2/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 
 
-//ii.Обновление XML документа.
 //iii.Запись (Добавление) в XML документ.
 namespace ConsoleApp1
 {
@@ -18,24 +17,40 @@ namespace ConsoleApp1
         {
             read();
             update();
+            add();
             Console.ReadLine();
         }
 
         static void read()
         {
-            XDocument xdoc = XDocument.Load("../../try1.xml");
+            Console.WriteLine("\nREAD XML \n");
+            XDocument xdoc = XDocument.Load("../../working.xml");
             var query = from Lines in xdoc.Descendants("Lines")
                         select Lines.Value;
             foreach (var item in query)
-                Console.WriteLine(item);  
+                Console.WriteLine(item + "  ");  
         }
 
         static void update()
         {
-            XDocument xdoc = XDocument.Load("../../try2.xml");
-            xdoc.Element("Lines").Element("Line").SetValue("asf");
-            Console.WriteLine(xdoc.Element("Lines").Element("Line"));
+            Console.WriteLine("\nUPDATE XML \n");
+            XDocument xdoc = XDocument.Load("../../working.xml");
+            xdoc.Element("Lines").Element("Line").Element("LineName").SetValue("asf");
+            Console.WriteLine(xdoc.Element("Lines").Element("Line").Element("LineName"));
         }
 
+        static void add()
+        {
+            Console.WriteLine("\nADD TO XML \n");
+            XDocument xdoc = XDocument.Load("../../working.xml");
+            XElement elementAdd = new XElement("Line", "newlinename");
+            xdoc.Element("Lines").Add(elementAdd);
+
+            var query = from res in xdoc.Descendants("Line")
+                        select res.Value;
+
+            foreach (var item in query)
+                Console.WriteLine(item + "  ");
+        }
     }
 }
